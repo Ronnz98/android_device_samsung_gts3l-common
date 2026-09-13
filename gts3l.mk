f27f9285f032b201d3780d229e2a3f16a6fa093d
Ronnz98 <56398486+Ronnz98@users.noreply.github.com>
2026-09-13 22:14:22 +0200

    Update gts3l.mk

--- gts3l.mk
+++ gts3l.mk
@@ -135,7 +135,6 @@ PRODUCT_PACKAGES += \
     android.frameworks.displayservice@1.0 \
     android.hardware.camera.provider@2.4-impl:32 \
     android.hardware.camera.provider@2.4-service \
-    vendor.qti.hardware.camera.device@1.0 \
     libshims_cameraclient
 
 PRODUCT_COPY_FILES += \
